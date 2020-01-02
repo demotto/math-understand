20f591a6c05b7739eb49949297a816a3e8a5bb86
胡一帆 <yifan.hu@geely.com>
2020-01-02 15:45:42 +0800

    AAAAA

--- jupyter/build_image.sh
+++ jupyter/build_image.sh
@@ -7,5 +7,3 @@ cd "$(dirname "$0")"
 docker build -t "${full_image_name}" .
 docker push "$full_image_name"
 
-# Output the strict image name (which contains the sha256 image digest)
-#docker inspect --format="{{index .RepoDigests 0}}" "${IMAGE_NAME}"
