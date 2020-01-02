a22a8e20a3a3d6ad39e86383d8638f2e2da5f71b
胡一帆 <yifan.hu@geely.com>
2020-01-02 15:34:59 +0800

    AAAAA

--- jupyter/start.sh
+++ jupyter/start.sh
@@ -1,4 +1,5 @@
 BASE_DIR=$(cd `dirname $0`/..; pwd)
 WORK_DIR=${ROOT_DIR}/work
-chmod -R 777 WORK_DIR
+echo $WORK_DIR
+chmod -R 777 ${WORK_DIR}
 docker run -d -p 8888:8888 -v ${WORK_DIR}:/home/jovyan/work  172.17.198.145:5000/my-jupyter
